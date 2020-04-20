fe144ab21e6f778eb7f0d20ae7e957899d4f0273
Chris on Code <chris@scotch.io>
2020-04-20 04:39:20 -0700

    markdown editor start is cleared. oops

--- markdown-editor/start/src/App.js
+++ markdown-editor/start/src/App.js
@@ -1,25 +1,12 @@
-import React, { useState } from 'react';
-import marked from 'marked';
-import ReactMarkdown from 'react-markdown';
+import React from 'react';
 import './App.css';
 
 export default function App() {
-  const [markdown, setMarkdown] = useState('# suppppp');
-
-  function handleChange(e) {
-    setMarkdown(e.target.value);
-  }
-
   return (
     <div className="app">
-      <textarea onChange={handleChange} value={markdown} />
+      <textarea />
 
-      {/* <div
-        className="preview"
-        dangerouslySetInnerHTML={{ __html: marked(markdown) }}
-      /> */}
-
-      <ReactMarkdown className="preview" source={markdown} />
+      <div className="preview" />
     </div>
   );
 }
