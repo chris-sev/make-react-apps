a5e5d106f5f2feb7121ef3c637d57015b40e5fd9
Chris on Code <chris@scotch.io>
2020-05-07 11:16:38 -0700

    for real this time

--- authentication/start/src/index.js
+++ authentication/start/src/index.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { Auth0Provider } from './contexts/auth0-context';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Auth0Provider>
-      <App />
-    </Auth0Provider>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
