88e20d1287ed161fd9c6aaee0cc2d0d75aca901d
Chris on Code <chris@scotch.io>
2020-06-01 14:35:38 -0700

    chat box starter

---
+++ chat-box/start/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './App.css';
+
+const messages = [
+  { text: 'How do I get better at React?' },
+  { text: 'Just build something!' },
+  { text: 'OK! What should I build?' },
+  { text: 'Iono. Just Google it?' },
+  { text: 'Oh! This course looks cool!' },
+  { text: 'Send me the link?!' },
+  { text: '20ReactApps.com!' },
+];
+
+export default function App() {
+  return (
+    <div className="app">
+      <div className="walkthrough">
+        {messages.map((message, index) => {
+          return <Message key={index} message={message} />;
+        })}
+      </div>
+    </div>
+  );
+}
+
+function Message({ message }) {
+  return <div className="message">{message.text}</div>;
+}
