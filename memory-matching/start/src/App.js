0caf4bc813fb102493bc55a880511e11aae377d3
Chris on Code <chris@scotch.io>
2020-06-05 10:48:10 -0700

    memory-match start ready

---
+++ memory-matching/start/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+const pokemon = [
+  { id: 4, name: 'charizard' },
+  { id: 10, name: 'caterpie' },
+  { id: 77, name: 'ponyta' },
+  { id: 108, name: 'lickitung' },
+  { id: 132, name: 'ditto' },
+  { id: 133, name: 'eevee' },
+];
+
+export default function App() {
+  return <div className="app"></div>;
+}
