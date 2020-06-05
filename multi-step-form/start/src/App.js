955aa98f4be7bbd9400c1dee60c65a8102ad988b
Chris on Code <chris@scotch.io>
2020-06-05 08:21:41 -0700

    multi-step-form really ready

--- multi-step-form/start/src/App.js
+++ multi-step-form/start/src/App.js
@@ -1,11 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
 
 export default function App() {
-  return (
-    <Router>
-      <div className="app">Signup form goes here</div>
-    </Router>
-  );
+  return <div className="app">Signup form goes here</div>;
 }
