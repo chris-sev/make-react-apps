d4ea6bf9091687bc8bb2c92ce0acd1bdedbd3cc8
Chris on Code <chris@scotch.io>
2020-06-24 16:05:03 -0700

    weather chart start ready

---
+++ weather-chart/start/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="app">
+      {/* form goes here */}
+      {/* chart goes here */}
+    </div>
+  );
+}
