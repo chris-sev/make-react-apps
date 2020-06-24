fc0908c5508aee3f9b20524cad0e376c965bb064
Chris on Code <chris@scotch.io>
2020-06-24 13:23:12 -0700

    real time search start done

---
+++ real-time-search/start/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+// ===== OPTION 1: if you want to create your own algolia account and fill it with data =====
+// demo data: https://www.algolia.com/doc/guides/building-search-ui/resources/demos/react/#media
+// sample datasets on github: https://github.com/algolia/datasets
+
+// ===== OPTION 2: if you want to get started quickly =====
+// app id: SSN9CKZUJ2
+// api key: ae8a3f7cff93d40282a4831930bfa38b
+// index name: products
+
+export default function App() {
+  return (
+    <div className="app">
+      <div className="search-container"></div>
+    </div>
+  );
+}
