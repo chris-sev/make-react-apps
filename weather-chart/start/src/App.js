e643e425c4d4536dfa217e0d139fda1cbd780fbe
Chris on Code <chris@scotch.io>
2020-06-24 16:08:30 -0700

    weather start ready

--- weather-chart/start/src/App.js
+++ weather-chart/start/src/App.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import './App.css';
 
+// openweathermap.org
+// https://api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689&exclude=hourly,minutely&units=imperial&appid=bb96c7f9ac6f57dc00333727c5407547
+// api key: bb96c7f9ac6f57dc00333727c5407547
+
+// google maps api
+// api key: AIzaSyDp4os33WF6-4d-xFVyL0HsUUHN7dOml_w
+
 export default function App() {
   return (
     <div className="app">
