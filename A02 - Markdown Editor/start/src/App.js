b5eb4e47ea19d52e70c52695b4a7c30e2ef94df5
d0op <jimmy_svensson91@hotmail.com>
2020-09-06 09:00:27 +0200

    2
    2

--- A02 - Markdown Editor/start/src/App.js
+++ A02 - Markdown Editor/start/src/App.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
+import marked from 'marked';
 import './App.css';
 
 export default function App() {
+  const [markdownm, setMarkdown] = useState('# sup');
+
+  function handleChange(e) {
+    setMarkdown(e.target.value);
+  }
+
   return (
     <div className="app">
-      <textarea />
+      <textarea onChange={handleChange} value={markdownm} />
 
-      <div className="preview" />
+      <div
+        className="preview"
+        dangerouslySetInnerHTML={{ __html: marked(markdownm) }}
+      />
     </div>
   );
 }
