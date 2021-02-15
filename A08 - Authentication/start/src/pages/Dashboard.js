31506b8960186c920420da9673e194b0312ff620
Gobinath V <61287591+Gobinath24@users.noreply.github.com>
2021-02-15 01:25:33 -0800

    Update Dashboard.js
    changing error in display

--- A08 - Authentication/start/src/pages/Dashboard.js
+++ A08 - Authentication/start/src/pages/Dashboard.js
@@ -6,7 +6,7 @@ export default function Dashboard() {
     <div className="page dashboard">
       <div>
         <img src={Frodo} alt="Frodo" />
-        <h2>Welcome Persom</h2>
+        <h2>Welcome Person</h2>
       </div>
     </div>
   );
